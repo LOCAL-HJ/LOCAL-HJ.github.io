6b9ea54cab87d37915b7e7f146a124a1bc6afc72
LOCAL-HJ <LOCAL-HJ@users.noreply.github.com>
2024-11-28 10:41:01 +0000

    deploy: 90eddffac92b0db3c30995bd668b8c8afb88e429

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Jekyll] 블로그 포스팅 ",
+        "excerpt":"로컬 Jekyll 서버 실행 방법   Jekyll 은 정적 사이트 생성기로, 로컬에서 테스트 서버를 실행하여 웹사이트가 어떻게 보일지 확인할 수 있다.     로컬 서버 실행   bundle exec jekyll serve  ","categories": [],
+        "tags": ["jekyll","Github"],
+        "url": "/test/",
+        "teaser": null
+      }]
